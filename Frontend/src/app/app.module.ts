11f6c00f74e12ca4cb3589d3c140ebdb81b5c484
ilya <levin.8@yandex.ru>
2023-02-09 00:29:21 +0300

    Add new Frontend

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthClientComponent } from './auth/auth-client/auth-client.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthClientComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
